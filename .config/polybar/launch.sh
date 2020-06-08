869e30d66c526b892d2ee829e4ae277a0192f5a1
mrwhoknows55 <avdhuttanugade55@gmail.com>
2020-06-08 20:48:14 +0530

    Added i3 and polybar configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload example  &
+done
+
+echo "Bars launched..."
